feea2839bc5f61d4ad0d648d1ed2e4b470c307b7
Casta <palolahrn0104@gmail.com>
2020-09-28 17:17:14 -0500

    Se crea proyecto en react, sin modificar

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Inicio from './paginas/Inicio';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Inicio />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
